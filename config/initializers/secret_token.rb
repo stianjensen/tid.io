f70bfd8845531fda31e0691c754af75cc29233f5
Stian Jensen <me@stianj.com>
2013-08-31 00:04:15 +0200

    Setup app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TidIo::Application.config.secret_key_base = '24eebdd138396afbcb27669b68de6ee18593ef4210c494a9985cce3fa1d51544e832e0c5f0bb6857ab5ca625358f0679485eb5ab051a41ad7dba470bd5757f99'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TidIo::Application.config.session_store :cookie_store, key: '_tid_io_session'
